7738d5d119596b75e1e3cc582cce838f98d25e4f
tphillips <tris.phillips@gmail.com>
2014-01-03 17:05:06 +0000

    Update DBMemberAttribute.cs

--- SERV.Utils/Data/DBMemberAttribute.cs
+++ SERV.Utils/Data/DBMemberAttribute.cs
@@ -7,7 +7,7 @@ namespace SERV.Utils.Data
 		public int MaxLength { get; set; }
 		public bool KeyField { get; set; }
 		/// <summary>
-		/// When set and true, calls ODHR.Utils.String.MakeSafeHtmlTags on the value of the property inside ODHR.Utils.String.GetStringFromDbMember so that it can be rectified before being written to the database.
+		/// When set and true, calls SERV.Utils.String.MakeSafeHtmlTags on the value of the property inside SERV.Utils.String.GetStringFromDbMember so that it can be rectified before being written to the database.
 		/// </summary>
 		public bool EnsureHtmlSafeOnSave { get; set; }
 	}
